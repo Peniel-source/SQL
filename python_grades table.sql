44bfb3295972f5e547b1c31956948e4dffd7a9f6
EKIRABO <e.kirabo@alustudent.com>
2025-08-21 21:37:20 +0200

    Delete python_grades table.sql

--- python_grades table.sql
+++
@@ -1,9 +0,0 @@
--- Create table for Python course grades
-CREATE TABLE python_grades (
-    course_id INT,
-    course_name VARCHAR(100),
-    student_id INT,
-    grade_obtained DECIMAL(5,2),
-    PRIMARY KEY (student_id, course_id),
-    FOREIGN KEY (student_id) REFERENCES students(student_id)
-);
