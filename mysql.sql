ab2025950d3d8d450c8bef24918ce676f5ee2209
Esther <e.kirabo@alustudent.com>
2025-08-21 21:17:21 +0200

    Linux grades

--- mysql.sql
+++ mysql.sql
@@ -4,3 +4,12 @@ CREATE TABLE students (
     student_name VARCHAR(100),
     intake_year INT
 );
+Create linux_grades table to put student details for the linux course
+CREATE TABLE linux_grades (
+    course_id INT,
+    course_name VARCHAR(100),
+    student_id INT,
+    grade_obtained DECIMAL(5,2),
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
